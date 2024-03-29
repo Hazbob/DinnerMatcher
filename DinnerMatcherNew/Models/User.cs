ba4ebbc47c44c7d4a23d4273c9ff0c6d41c6cd4a
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2024-03-29 22:44:01 +0000

    Initial Database setup

---
+++ DinnerMatcherNew/Models/User.cs
@@ -0,0 +1,11 @@
+namespace DinnerMatcherNew.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public IEnumerable<User>? Friends { get; set; }
+    }
+}
